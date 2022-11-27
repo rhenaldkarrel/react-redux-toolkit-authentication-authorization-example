b9c8eb259c01679bf081623d8d9b68ca543a49ed
Rhenald <renaldkarel@gmail.com>
2022-11-27 15:37:22 +0700

    services: auth and user
    * install axios
    * adjust lint-staged

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,56 @@
+import axios from "axios";
+import type { AxiosResponse } from 'axios';
+
+interface ICredentials {
+  username: string;
+  password: string;
+  email: string;
+}
+
+type TLogin = Pick<ICredentials, 'password' | 'username'>;
+
+const API_URL = "http://localhost:3000/api/auth/";
+
+const register = ({ username, email, password }: ICredentials) => {
+  return axios
+    .post(API_URL + "signup", {
+      username,
+      password,
+      email,
+    });
+}
+
+const login = ({ username, password }: TLogin) => {
+  return axios
+    .post(API_URL + "signin", {
+      username,
+      password,
+    })
+    .then((res: AxiosResponse<TLogin>) => {
+      if (res.data.username) {
+        localStorage.setItem("user", JSON.stringify(res.data));
+      }
+
+      return res.data;
+    });
+};
+
+const logout = () => {
+  localStorage.removeItem("user");
+  return axios
+    .post(API_URL + "signout")
+    .then(res => res.data);
+};
+
+const getCurrentUser = () => {
+  return JSON.parse(localStorage.getItem("user") ?? "");
+};
+
+const AuthService = {
+  register,
+  login,
+  logout,
+  getCurrentUser,
+};
+
+export default AuthService;
