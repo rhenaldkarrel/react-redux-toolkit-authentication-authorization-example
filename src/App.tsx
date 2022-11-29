78e4feb209aadd2cf4aae559dc9a0893564f6a7b
Rhenald <renaldkarel@gmail.com>
2022-11-29 10:28:24 +0700

    feat: add profile page

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Login from 'pages/Login/Login';
 import Register from 'pages/Register/Register';
+import Profile from 'pages/Profile/Profile';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/profile" element={<Profile />} />
     </Routes>
   );
 }
---
+++ src/pages/Profile/Profile.tsx
@@ -0,0 +1,29 @@
+import { Navigate } from 'react-router-dom';
+import { useAppSelector } from 'app/hooks';
+
+const Profile = () => {
+  const { user: currentUser } = useAppSelector(state => state.auth);
+
+  if (!currentUser) {
+    return <Navigate to="/" />;
+  }
+
+  return (
+    <div className="grid justify-center items-center h-screen">
+      <div className="bg-white md:w-96 p-9 rounded-xl shadow-xl m-4">
+        <h1>{currentUser.username}</h1>
+        <p>ID: {currentUser.id}</p>
+        <p>Email: {currentUser.email}</p>
+        <strong>Authorities:</strong>
+        <ul>
+          {currentUser.roles &&
+            currentUser.roles.map((role: any, index: number) => (
+              <li key={index}>{role}</li>
+            ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
