aa32dc405f52045c22bc2b67b4313682fdce57a8
Rhenald <renaldkarel@gmail.com>
2023-01-17 16:22:41 +0700

    feat: add home page and route

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import Login from 'pages/Login/Login';
 import Register from 'pages/Register/Register';
 import Profile from 'pages/Profile/Profile';
+import Home from 'components/Home';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/profile" element={<Profile />} />
+      <Route path="/home" element={<Home />} />
     </Routes>
   );
 }
---
+++ src/components/Home.tsx
@@ -0,0 +1,29 @@
+import UserService from 'app/services/user.service';
+import { useEffect, useState } from 'react';
+
+const Home = () => {
+  const [content, setContent] = useState('');
+
+  useEffect(() => {
+    UserService.getPublicContent()
+      .then(res => setContent(res.data))
+      .catch(error => {
+        const _content =
+          (error.response && error.response.data) ||
+          error.message ||
+          error.toString();
+
+        setContent(_content);
+      });
+  }, []);
+
+  return (
+    <div className="container h-screen bg-green-100 grid place-items-center">
+      <header className="text-3xl font-bold">
+        <h3>{content}</h3>
+      </header>
+    </div>
+  );
+};
+
+export default Home;
