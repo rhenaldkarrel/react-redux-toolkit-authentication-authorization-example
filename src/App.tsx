7b010315e9eb88326505ff4255dd037a7850ee20
Rhenald <renaldkarel@gmail.com>
2023-01-17 16:56:13 +0700

    feat: add navbar and routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,25 @@ import Login from 'pages/Login/Login';
 import Register from 'pages/Register/Register';
 import Profile from 'pages/Profile/Profile';
 import Home from 'components/Home';
+import Navbar from 'components/Navbar';
+import BoardModerator from 'components/BoardModerator';
+import BoardAdmin from 'components/BoardAdmin';
+import BoardUser from 'components/BoardUser';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/home" element={<Home />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/moderator" element={<BoardModerator />} />
+        <Route path="/admin" element={<BoardAdmin />} />
+        <Route path="/user" element={<BoardUser />} />
+      </Routes>
+    </>
   );
 }
 
