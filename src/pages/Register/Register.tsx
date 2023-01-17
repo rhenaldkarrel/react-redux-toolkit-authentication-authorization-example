59591d06291d904bd55218cead1e732fa6717cd9
Rhenald <renaldkarel@gmail.com>
2023-01-17 16:14:25 +0700

    styling: adjust alert state

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import { Formik, Field, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import { useAppDispatch, useAppSelector } from 'app/hooks';
+import classnames from 'classnames';
 
 import { register } from 'app/slices/auth';
 import { clearMessage } from 'app/slices/message';
@@ -143,21 +144,29 @@ export const Register = () => {
         </div>
         {message && (
           <div
-            className="mt-8 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded flex justify-between"
+            className={classnames(
+              'mt-8 border px-4 py-3 rounded flex justify-between',
+              {
+                'bg-red-100 border-red-400 text-red-700': !successful,
+                'bg-green-100 border-green-400 text-green-700': successful,
+              },
+            )}
             role="alert"
           >
             <span className="block sm:inline">{message}</span>
-            <span>
-              <svg
-                className="fill-current h-6 w-6 text-red-500"
-                role="button"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-              >
-                <title>Close</title>
-                <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
-              </svg>
-            </span>
+            {!successful && (
+              <span>
+                <svg
+                  className="fill-current h-6 w-6 text-red-500"
+                  role="button"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <title>Close</title>
+                  <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+                </svg>
+              </span>
+            )}
           </div>
         )}
       </div>
