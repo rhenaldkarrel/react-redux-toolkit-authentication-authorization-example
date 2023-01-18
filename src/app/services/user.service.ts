fd0dfbbaeebfeda5010be36a0f76991a9540167c
Rhenald <renaldkarel@gmail.com>
2023-01-18 12:28:16 +0700

    feat: migrate from session token to localStorage

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,21 +1,27 @@
 import axios from 'axios';
 
 const API_URL = 'http://localhost:8080/api/test/';
+const USER = JSON.parse(localStorage.getItem('user') || '{}');
+const HEADERS = {
+  headers: {
+    authorization: 'Bearer ' + USER.token,
+  },
+};
 
 const getPublicContent = () => {
   return axios.get(API_URL + 'all');
 };
 
 const getUserBoard = () => {
-  return axios.get(API_URL + 'user');
+  return axios.get(API_URL + 'user', HEADERS);
 };
 
 const getModeratorBoard = () => {
-  return axios.get(API_URL + 'mod');
+  return axios.get(API_URL + 'mod', HEADERS);
 };
 
 const getAdminBoard = () => {
-  return axios.get(API_URL + 'admin');
+  return axios.get(API_URL + 'admin', HEADERS);
 };
 
 const UserService = {
